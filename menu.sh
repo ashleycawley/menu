52f7394d96b23988239778c0da1bdc7ce46a5d37
Ashley Cawley <acawley@cloudabove.com>
2018-09-26 16:53:36 +0100

    Added easy imap sync tool

--- menu.sh
+++ menu.sh
@@ -5,7 +5,7 @@ set -u
 OPTION=$(whiptail --title "cloudabove Toolkit v0.2" --menu "Choose your option" 15 60 8 \
 "1" "Migrate (Site & email data cP > cP via FTP or RSYNC)" \
 "2" "Report (Custom DNS Report)" \
-"3" "Work in progress" \
+"3" "Easy IMAP Sync" \
 "4" "Disk Space Tools" \
 "5" "Exit"  3>&1 1>&2 2>&3)
 
@@ -19,7 +19,7 @@ fi
 case $OPTION in
     1) migrate ;;
     2) report ;;
-    3) echo "Invalid option. Quitting";;
+    3) easyimapsync ;;
     4) dstools ;;
     *) exit 0;;
 esac
